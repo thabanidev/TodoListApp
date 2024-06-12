d8d40ed01fb82232ba24e2d517a18c2c12bbe2a3
Thabani Takwena <thabanidev@gmail.com>
2024-06-12 04:39:13 +0200

    Add project files.

---
+++ TodoListApp/Models/TodoItem.cs
@@ -0,0 +1,10 @@
+﻿namespace TodoListApp.Models
+{
+    public class TodoItem
+    {
+        public string Title { get; set; } = string.Empty;
+        public bool IsDone { get; set; } = false;
+        public DateTime DateCreated { get; set; } = DateTime.Now;
+        public DateTime? DateEdited { get; set; }
+    }
+}
